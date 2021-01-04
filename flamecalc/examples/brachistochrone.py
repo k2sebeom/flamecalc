eeee2eacf4472a96ca59cde3e5c16440182b090d
k2sebeom <moses97@gmail.com>
2021-01-04 23:52:01 +0900

    Refine examples

---
+++ flamecalc/examples/brachistochrone.py
@@ -0,0 +1,47 @@
+import torch
+from torch.optim import Adam
+import matplotlib.pyplot as plt
+
+from flamecalc import CalVarSolver
+from math import pi
+
+
+def f(y, dy, x):
+    return torch.sqrt((1 + dy**2) / (-y + 0.000001))
+
+
+if __name__ == '__main__':
+    A = (0, 0)
+    B = (pi, -2)
+    domain = torch.linspace(A[0], B[0], 20000)
+    model = CalVarSolver(f, A, B, domain)
+
+    initial_theta = [-1.0] + [0] * 20
+    theta = torch.tensor([initial_theta], requires_grad=True).float()
+
+    optimizer = Adam([theta], lr=0.002)
+
+    losses = []
+    epoch = 300
+
+    for i in range(epoch):
+        result = model(theta)
+        ys, dys = result["y"], result["dy"]
+        loss = result["result"]
+        optimizer.zero_grad()
+        print(f"Epoch {i +1}/{epoch}: {loss}", end='\r')
+        loss.backward()
+        optimizer.step()
+        losses.append(loss.detach().numpy()[0])
+        if i % 50 == 0:
+            plt.plot(domain, ys.detach().numpy()[0])
+    plt.show()
+    plt.plot(domain, ys.detach().numpy()[0])
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.show()
+
+    plt.plot(range(epoch), losses)
+    plt.xlabel("epoch")
+    plt.ylabel("value")
+    plt.show()
---
+++ flamecalc/examples/line.py
@@ -0,0 +1,29 @@
+import torch
+import matplotlib.pyplot as plt
+from flamecalc import CalVarSolver
+
+
+def f(y, dy, x):
+    return torch.sqrt(1 + dy**2)
+
+
+if __name__ == '__main__':
+    A = (1, 1)
+    B = (2, 5)
+    domain = torch.linspace(A[0], B[0], 100)
+
+    model = CalVarSolver(f, A, B, domain)
+    epoch = 2500
+    result = model.optimize(lr=0.02, epoch=epoch)
+
+    y = result["y"].detach()[0]
+    losses = result["log"]
+    plt.plot(domain, y)
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.show()
+
+    plt.plot(range(epoch), losses)
+    plt.xlabel("epoch")
+    plt.ylabel("value")
+    plt.show()
